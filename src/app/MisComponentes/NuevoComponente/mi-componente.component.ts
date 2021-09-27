518289691896365626cd84ab8f3527558e4c3f5f
Dpablook <pablodario2014@gmail.com>
2021-09-27 08:48:22 -0300

    1 file modified, 1 added

--- src/app/MisComponentes/NuevoComponente/mi-componente.component.ts
+++ src/app/MisComponentes/NuevoComponente/mi-componente.component.ts
@@ -2,22 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mi-componente',
-  template: `
-  <nav class="nav">
-    <div class="logo">
-      <img src="https://cdn.iconscout.com/icon/free/png-256/angular-5-457929.png" class="imagen">
-    </div>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Nosotros</a></li>
-        <li><a href="#">Menu</a></li>
-        <li><a href="#">Promociones</a></li>
-        <li><a href="#">Contacto</a></li>
-      </ul>
-  </nav>
-  <h1>prueba superada</h1>
-  `
+  templateUrl: `./mi-componente.component.html`
 })
+
 export class MiComponente{
     constructor(){
       console.log("Componente mi-componente cargado");
